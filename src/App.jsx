5a115cfd6220d727998a06f44f35ef4e268da6a9
Luis Carlin <luiscarlin@gmail.com>
2018-04-24 06:22:18 -0400

    display circles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import SmallCircle from './SmallCircle.jsx'
 
 export default class App extends Component {
   render () {
+
+    const container = {
+      display: 'flex',
+      flexDirection: 'column'
+      
+    }
+
     return (
-      <div>
-        <div>
+      <div style={container}>
           <SmallCircle color='#0d4261'/>
-        </div>
+          <SmallCircle color='#0d4261'/>
+          <SmallCircle color='#0d4261'/>
+          <SmallCircle color='#0d4261'/>
+          <SmallCircle color='#0d4261'/>
+          <SmallCircle color='#0d4261'/>
       </div>
     )
   }
